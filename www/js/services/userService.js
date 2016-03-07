9892555ab80d24a98043a79a0bc16d9862ef8a27
jiandongli <1989lijiandong@gmail.com>
2016-03-07 19:28:06 +1100

    update merchantService
    copy merchantAllHtmlHtmlHelper from userService to merchantService

--- www/js/services/userService.js
+++ www/js/services/userService.js
@@ -242,15 +242,6 @@ define(['jquery', 'framework7','underscore'], function ($, Framework7,_) {
         return content;
     };
 
-    var merchantAllHtmlHtmlHelper = function(v){
-        var content = '<li> <div class="col-100 tablet-50"> <div class="tc-product"><a href=shopdetail.html?shopId=' + v.merchantId + 'class="title">';
-        content += '<img src=' + imgBaseUrl + v.logoUrl + '/><div class="details"> <div class="head"><h3>' + v.merchantName + '</h3>';
-        content += '<h3>Chatswood</h3> </div> <div class="buttons"> <a href="#"><i class="uiicon-web39 color-orange"></i> Your points:' + v.userPoints + '</a>';
-        content += '<a href="shopdetail.html?shopId=' + v.merchantId + '><i class="uiicon-web38"></i> Details</a>';
-        content += '</div></div></a></div></div></li>';
-        return content;
-    };
-
     return{
         theApp : theApp,
         getMerchantCount:getMerchantCount,
